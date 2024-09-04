f61b31ecf8ede9f24a5143b7eb0e40606d09e04e
s-sweta <swetasingh84478@gmail.com>
2024-09-04 15:39:20 +0530

    Made client and server directory

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+    <div
+      style={{
+        color: 'white',
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <h1>Hello! Developer</h1>
+    </div>
+  )
+}
+
+export default App
